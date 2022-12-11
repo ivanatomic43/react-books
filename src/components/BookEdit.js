95d8ad0c829f43b68cabafa713080da5d10f565b
ivanatomic43 <ivana.tomic96@gmail.com>
2022-12-11 17:49:35 +0100

    React books app

---
+++ src/components/BookEdit.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useState } from "react";
+import useBooksContext from "../hooks/use-books-context";
+
+
+function BookEdit({book, onSubmit}) {
+
+    const [title, setTitle] = useState(book.title);
+    const {editBookById} = useBooksContext();
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSubmit(); //closes the form
+        editBookById(book.id, title);
+    };
+
+    return(
+        <form className="book-edit" onSubmit={handleSubmit}>
+            <label>Title</label>
+            <input className="input"
+                    value={title}
+                    onChange={handleChange}
+            />
+            <button className="button is-primary">Save</button>
+        </form>
+    );
+}
+
+export default BookEdit;
---
+++ src/components/BookList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import BookShow from './BookShow';
+import useBooksContext from "../hooks/use-books-context";
+
+
+function BookList() {
+
+    const { books } = useBooksContext();
+
+    const renderBooks = books.map((book) => {
+       return <BookShow key={book.id} book={book} />
+    });
+
+    return(
+        <div className="book-list">
+            {renderBooks}
+        </div>
+    );
+}
+
+export default BookList;
